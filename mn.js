c48d988574afe7a3399ee65a2bc50ad322a5c9ce
Hunter Cote <hcote12@gmail.com>
2019-07-20 18:09:06 -0700

    changing keys file

--- mn.js
+++
@@ -1,3 +0,0 @@
-const mnemonic = "cross chat obtain casino frame hen tape load bone desert sun illness"
-
-export default mnemonic;
--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HDWalletProvider = require("truffle-hdwallet-provider");
 // test wallet ONLY - contains no actual ether
-import mnemonic from "./mn.js"
+import {mnemonic, key} from "./keys.js"
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -13,13 +13,13 @@ module.exports = {
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/d26fa7f5deca4f8d952d53ca9a8d71ce")
+        return new HDWalletProvider(mnemonic, key)
       },
       network_id: 3
     },
     main: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/d26fa7f5deca4f8d952d53ca9a8d71ce")
+        return new HDWalletProvider(mnemonic, key)
       },
       network_id: 1
     },
