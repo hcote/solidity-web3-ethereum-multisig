8e03e90290451a16ca49181bc95548c417872e83
Hunter Cote <hcote12@gmail.com>
2019-07-21 12:15:54 -0700

    pushing to heroku

--- truffle-config.js
+++ truffle-config.js
@@ -1,23 +1,23 @@
-const path = require("path");
-const HDWalletProvider = require("truffle-hdwallet-provider");
+// const path = require("path");
+// const HDWalletProvider = require("truffle-hdwallet-provider");
 
-module.exports = {
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
-  networks: {
-    develop: {
-      port: 8545
-    },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/")
-      },
-      network_id: 3
-    },
-    main: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/")
-      },
-      network_id: 1
-    },
-  }
-};
+// module.exports = {
+//   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+//   networks: {
+//     develop: {
+//       port: 8545
+//     },
+//     ropsten: {
+//       provider: function() {
+//         return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/")
+//       },
+//       network_id: 3
+//     },
+//     main: {
+//       provider: function() {
+//         return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/")
+//       },
+//       network_id: 1
+//     },
+//   }
+// };
